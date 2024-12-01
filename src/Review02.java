a098ccf649b00a1a1b5434095bf3bf1d11f3709c
kenkowt <1371x118.nole.kenkow@gmail.com>
2024-12-01 21:26:39 +0900

    if文の1番初めの条件の修正 i % 5 == 7 ⇒i % 5 == 0

--- src/Review02.java
+++ src/Review02.java
@@ -2,7 +2,7 @@ public class Review02 {
 
     public static void main(String[] args) {
         for (int i = 1; i <= 100; i++) {
-            if (i % 3 == 0 && i % 5 == 7) {
+            if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println("FizzBuzz");
             } 
             else if (i % 3 == 0) {
